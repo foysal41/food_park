d41c71a5e0e8d21e62c96e98216c6e7d8bec1e22
foysal41 <mdfoysalmf33@gmail.com>
2024-11-04 18:23:48 +0600

    Multi Auth - Add New Seeder for user and admin.

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::insert([
+            ['name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'role' => 'admin',
+            'password' => 'admin1234'//password
+        ],
+
+        ['name' => 'User',
+            'email' => 'user@gmail.com',
+            'role' => 'user',
+            'password' => 'user1234'//password
+        ],
+        ]);
+    }
+}
