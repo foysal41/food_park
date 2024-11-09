cd42ed4d62ecac67e52f1de290536e57e9d06284
foysal41 <mdfoysal.mf33@gmail.com>
2024-11-09 17:02:03 +0600

    Admin Setup - Admin Panel Mastering (Part - 3)

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -9,11 +9,6 @@
 <link rel="stylesheet" href="{{ asset('admin/assets/modules/bootstrap/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
 
-<!-- CSS Libraries -->
-<link rel="stylesheet" href="{{ asset('admin/assets/modules/jqvmap/dist/jqvmap.min.css') }}">
-<link rel="stylesheet" href="{{ asset('admin/assets/modules/weather-icon/css/weather-icons.min.css') }}">
-<link rel="stylesheet" href="{{ asset('admin/assets/modules/weather-icon/css/weather-icons-wind.min.css') }}">
-<link rel="stylesheet" href="{{ asset('admin/assets/modules/summernote/summernote-bs4.css') }}">
 
 <!-- Template CSS -->
 <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
@@ -43,7 +38,7 @@
       </div>
       <footer class="main-footer">
         <div class="footer-left">
-          Copyright &copy; 2018 <div class="bullet"></div> Design By <a href="https://nauval.in/">Muhamad Nauval Azhar</a>
+          Copyright &copy; 2025 <div class="bullet"></div> Design By <a href="https://foysaljaman.com/">Foysal Jaman</a>
         </div>
         <div class="footer-right">
 
@@ -58,19 +53,12 @@
 <script src="{{ asset('admin/assets/modules/tooltip.js') }}"></script>
 <script src="{{ asset('admin/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('admin/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
-<script src="{{ asset('admin/assets/modules/moment.min.js') }}"></script>
 <script src="{{ asset('admin/assets/js/stisla.js') }}"></script>
 
 <!-- JS Libraries -->
-<script src="{{ asset('admin/assets/modules/simple-weather/jquery.simpleWeather.min.js') }}"></script>
-<script src="{{ asset('admin/assets/modules/chart.min.js') }}"></script>
-<script src="{{ asset('admin/assets/modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
-<script src="{{ asset('admin/assets/modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
-<script src="{{ asset('admin/assets/modules/summernote/summernote-bs4.js') }}"></script>
-<script src="{{ asset('admin/assets/modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
+
 
 <!-- Page Specific JS File -->
-<script src="{{ asset('admin/assets/js/page/index-0.js') }}"></script>
 
 <!-- Template JS Files -->
 <script src="{{ asset('admin/assets/js/scripts.js') }}"></script>
