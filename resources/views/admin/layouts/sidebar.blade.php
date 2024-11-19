0d2063a8eb68eb26d2bcd6bbad66afa20bbbc533
foysal41 <mdfoysal.mf33@gmail.com>
2024-11-19 15:30:39 +0600

    Admin - Admin Logout Feature

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -24,9 +24,34 @@
                     <i class="fas fa-cog"></i> Settings
                 </a>
                 <div class="dropdown-divider"></div>
-                <a href="#" class="dropdown-item has-icon text-danger">
-                    <i class="fas fa-sign-out-alt"></i> Logout
-                </a>
+
+
+
+{{-- এই code টুকু  view>layout>navigation থেকে নেয়া হয়েছে --}}
+
+{{--
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <x-dropdown-link :href="route('logout')"
+                            onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                        {{ __('Log Out') }}
+                    </x-dropdown-link>
+                </form>
+--}}
+
+
+
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+
+                    <a href="#" onclick="event.preventDefault(); this.closest('form').submit();" class="dropdown-item has-icon text-danger">
+                        <i class="fas fa-sign-out-alt"></i> Logout
+                    </a>
+                </form>
+
+
             </div>
         </li>
     </ul>
