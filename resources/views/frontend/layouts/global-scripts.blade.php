ec0643a38df5c44b7409ce915a2120e9640a64b4
foysal41 <mdfoysal.mf33@gmail.com>
2025-01-30 17:25:49 +0600

    Cart Feature - Adding Loader on Ajax Request

--- resources/views/frontend/layouts/global-scripts.blade.php
+++ resources/views/frontend/layouts/global-scripts.blade.php
@@ -5,6 +5,9 @@
         $.ajax({
             method: 'GET',
             url: '{{ route("load-product-modal", ":productId") }}' . replace(':productId' , productId),
+            beforeSend:function(){
+                $('.overlay').addClass('active');
+            },
             success: function(response){
                 //console.log(response); check করে দেখলাম
                 $('.load_product_modal_body').html(response);
@@ -12,6 +15,9 @@
             },
             error: function(xhr, status, error){
                 console.error(error);
+            },
+            complete:function(){
+                $('.overlay').removeClass('active');
             }
         });
     }
